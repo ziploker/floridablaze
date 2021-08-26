8521fb02f3ca4c74a9c3004d93d675df31d15b42
Alejandro Raffo <amsterdamAL@gmail.com>
2021-08-26 06:24:44 -0400

    votes

--- app/javascript/packs/comments.jsx
+++ app/javascript/packs/comments.jsx
@@ -439,6 +439,7 @@ function CommentSection(props){
                 if (status == "voteup_undo"){
                
                     current.innerText = parseInt(current.innerText) - 1
+                    current.parentElement.classList.remove('selected')
                 
                 }else if (status == "voteup_toggle"){
 
@@ -456,6 +457,10 @@ function CommentSection(props){
                 }else if (status == "voteup"){
 
                     current.innerText = parseInt(current.innerText) + 1
+                    // current.parentElement.className = current.parentElement.className + " selected"
+                    current.parentElement.classList.add('selected')
+
+                    
                 }
 
             }
@@ -669,12 +674,12 @@ function CommentSection(props){
     
     
     
-                        <VoteUp onClick={(e)=>{handleVoteUp(e, item.id)}}>
+                        <VoteUp className={liker(item.array_of_likers)} onClick={(e)=>{handleVoteUp(e, item.id)}}>
                             {/* <svg viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg"><path key={item.id + "path1"} data-id={ item.id + "path1"} d="M10.74.04a2.013 2.013 0 00-1.58 1.88c-.11 2.795-.485 4.45-2.283 6.946a1.272 1.272 0 00-1.065-.58h-4.55C.573 8.287 0 8.84 0 9.507v8.773c0 .667.572 1.218 1.263 1.218h4.55c.435 0 .821-.22 1.049-.548.263.204.506.387.758.533.417.24.887.384 1.532.45 1.29.128 3.403.032 8.283.052a.53.53 0 00.317-.113c1.224-.667 4.255-5.775 4.248-10.534-.026-1.138-.542-1.78-1.532-1.78H13.96c.388-2.47.131-4.738-.735-6.208C12.76.555 12.078.111 11.403.018a2.035 2.035 0 00-.663.022m2.154 7.912c-.055.28.201.58.498.58h6.934c.356.035.67.091.67.913 0 1.047-.168 2.886-1.031 5.057-.865 2.172-2.155 4.531-2.603 4.455-1.215.08-7.014.109-8.108 0-.556-.056-.818-.135-1.113-.306-.266-.152-.59-.423-1.066-.791v-7.6c2.349-2.88 2.979-5.302 3.096-8.3.338-1.495 1.702-1.082 2.179-.13.697 2.402.879 4.442.544 6.122M1.263 9.262h4.55c.148 0 .251.1.251.244v8.773c0 .144-.103.243-.252.243h-4.55c-.148 0-.251-.099-.251-.243V9.506c0-.144.103-.244.252-.244"></path></svg> */}
                             {console.log("ITEM IN QUESTION", item)}
 
                             
-                            <ThumbsUpSvg width="18px" viewBox="0 0 22 20"/>
+                            <ThumbsUpSvg  width="18px" viewBox="0 0 22 20"/>
                            
 
                             <span ref={addToVoteUpRefs} commentid={item.id}>{item.total_upvotes}</span>
@@ -682,7 +687,6 @@ function CommentSection(props){
                         </VoteUp>
     
                         <VoteDown onClick={(e)=>{handleVoteDown(e, item.id)}}>
-                            <svg viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg"><path key={item.id + "path2"} data-id={ item.id + "path2"} d="M11.26 19.96a2.013 2.013 0 001.58-1.881c.11-2.794.484-4.45 2.282-6.945.224.345.618.58 1.066.58h4.548c.692 0 1.264-.553 1.264-1.22V1.722c0-.668-.572-1.22-1.264-1.22h-4.548c-.436 0-.823.22-1.05.55a6.898 6.898 0 00-.759-.534c-.416-.24-.887-.384-1.531-.45C11.558-.06 9.445.037 4.564.017a.521.521 0 00-.316.114C3.023.796-.007 5.904 0 10.663c.025 1.138.541 1.78 1.532 1.78H8.04c-.39 2.47-.131 4.738.735 6.208.467.794 1.148 1.238 1.823 1.331a2.034 2.034 0 00.663-.022m-2.155-7.913c.056-.28-.202-.579-.497-.579H1.674c-.356-.035-.67-.091-.67-.913 0-1.047.166-2.886 1.031-5.057C2.9 3.326 4.19.967 4.638 1.044c1.214-.081 7.014-.109 8.108 0 .556.055.818.134 1.113.305.265.152.59.423 1.066.791v7.6c-2.349 2.88-2.979 5.302-3.096 8.3-.338 1.495-1.702 1.083-2.179.13-.697-2.402-.88-4.442-.545-6.123m11.631-1.309h-4.548c-.149 0-.252-.1-.252-.244V1.722c0-.144.103-.244.252-.244h4.548c.15 0 .253.1.253.244v8.772c0 .144-.103.244-.253.244"></path></svg>
                             <span ref={addToVoteDownRefs} commentid={item.id}>{item.total_downvotes}</span>
     
                         </VoteDown>
