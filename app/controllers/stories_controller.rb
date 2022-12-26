bb6d186ebd8fbeae7bfc829afa3f6901f90eaaa2
Alejandro Raffo <amsterdamAL@gmail.com>
2022-12-26 16:20:37 -0500

    save to switch branch

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -185,6 +185,89 @@ class StoriesController < ApplicationController
   def show
       
   end
+
+  def admin_login
+
+    @testData = "yomamma"
+  end
+
+  def create_admin  
+        
+    puts "in sessions#create_admin=================="
+
+    puts "user email is = " + params["user"]["email"].downcase
+    puts "user PW is = " + params["user"][:password].strip
+    #search for user email and try to auth...
+    user = User
+        .find_by(email: params["user"]["email"].downcase)
+        .try(:authenticate, params["user"][:password])
+
+        puts "user that was found is = " + user.inspect
+
+    
+    if user.present?
+
+        puts "user present? " + user.present?.to_s
+        
+        
+        
+        puts "-------------------------user was present"
+        if user.email_confirmed == "true"
+            
+            
+            puts "-------------------------user email confirmed"
+
+            puts "in gen token-------------"
+    
+            #user[:auth_token] = SecureRandom.urlsafe_base64
+            #user.save!
+
+            someRandomNumber = SecureRandom.urlsafe_base64
+
+            puts "about to update generated token---------"
+            user.update_columns(auth_token: someRandomNumber)
+
+
+            puts "just updated auth token to DB-------------"  
+
+                puts "gen token is =========== " + user[:auth_token].to_s
+            
+            if params["user"]["rememberMe"]
+
+
+                puts "------------------------rememberMe was checked"
+                cookies.permanent[:auth_token] = user.auth_token
+            else
+                puts "------------------------rememberMe was NOT checked"
+                cookies[:auth_token] = user.auth_token
+            end
+            
+            # puts "-------------------------and email is confirmed"
+            # session[:user_id] = user.id
+            # puts "-------------------------user.id is " + user.id.to_s
+            render json:{
+                
+                status: "green",
+                logged_in: true,
+                user: user,
+                error: {success: ["You have successfully logged in !!"]}
+            }
+            
+        else
+            render json: {
+                status: "pink", 
+                error: {auth: ["Account not active yet, check email and click link"]}
+            }
+        end
+
+    else
+        render json: {
+            status: "pink", 
+            error: {auth: ["Email or password is bad."]}
+        }
+    end
+end
+
   
   
   private
