cc01f2cd2c3bb49639e3f1bd60834bffac909658
Alejandro Raffo <amsterdamAL@gmail.com>
2023-01-26 23:07:04 -0500

    heroku

---
+++ db/migrate/20210324161446_create_communications.rb
@@ -0,0 +1,16 @@
+class CreateCommunications < ActiveRecord::Migration[6.1]
+  def change
+    create_table :communications do |t|
+      t.string        :date
+      t.string        :com_type
+      t.string        :recipient
+      t.string        :status
+      t.references    :user, foreign_key: true
+      t.string        :postgrid_id
+      t.json          :paypal_full_object, default: {}
+      t.json          :postgrid_full_object, default: {}
+      t.timestamps
+    end
+  end
+end
+
