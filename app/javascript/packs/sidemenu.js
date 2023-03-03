08c1dd47ef45297101f57a102ea53b31458a32d3
Alejandro Raffo <amsterdamAL@gmail.com>
2023-03-02 23:37:05 -0500

    add key

--- app/javascript/packs/sidemenu.js
+++ app/javascript/packs/sidemenu.js
@@ -5,74 +5,76 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const UL = styled.ul`
-	li {
-		cursor: pointer;
-	}
+  li {
+    cursor: pointer;
+  }
 `;
 
 function Menu(props) {
-	return (
-		<StyledSideMenu
-			openSideMenu={props.openSideMenu}
-			hamburgerScrolled={props.hamburgerScrolled}
-		>
-			<UL>
-				<li key={1} style={{ borderBottom: "2px orange solid" }}>
-					<a href="/">Home</a>
-				</li>
-				<li key={2} style={{ borderBottom: "2px orange solid" }}>
-					<a onClick={props.executeScrollForLookupSection}>Act</a>
-				</li>
-				<li key={3} style={{ borderBottom: "2px orange solid" }}>
-					<a href="#">Shop</a>
-				</li>
+  return (
+    <StyledSideMenu
+      openSideMenu={props.openSideMenu}
+      hamburgerScrolled={props.hamburgerScrolled}
+    >
+      <UL>
+        <li key={1} style={{ borderBottom: "2px orange solid" }}>
+          <a href="/">Home</a>
+        </li>
+        <li key={2} style={{ borderBottom: "2px orange solid" }}>
+          <a onClick={props.executeScrollForLookupSection}>Act</a>
+        </li>
+        <li key={3} style={{ borderBottom: "2px orange solid" }}>
+          <a href="#">Shop</a>
+        </li>
 
-				<li key={4}>
-					{props.userState.loggedInStatus == "LOGGED_IN"
-						? [
-								<a
-									style={{ fontSize: "4vw" }}
-									key={"a"}
-									onClick={props.handleLogOutClick}
-								>
-									{" "}
-									Logout{" "}
-								</a>,
-								<span style={{ fontSize: "4vw" }}>| </span>,
-								<Link style={{ fontSize: "4vw" }} key={"b"} to="/edit">
-									edit{" "}
-								</Link>,
-						  ]
-						: [
-								<a
-									style={{ fontSize: "4vw" }}
-									key={"c"}
-									onClick={props.doSomething}
-								>
-									{" "}
-									Login |
-								</a>,
-								<a
-									style={{ fontSize: "4vw" }}
-									key={"d"}
-									onClick={props.executeScrollForSection2}
-								>
-									{" "}
-									Signup
-								</a>,
-						  ]}{" "}
-				</li>
+        <li key={4}>
+          {props.userState.loggedInStatus == "LOGGED_IN"
+            ? [
+                <a
+                  style={{ fontSize: "4vw" }}
+                  key={"a"}
+                  onClick={props.handleLogOutClick}
+                >
+                  {" "}
+                  Logout{" "}
+                </a>,
+                <span key={"line"} style={{ fontSize: "4vw" }}>
+                  |{" "}
+                </span>,
+                <Link style={{ fontSize: "4vw" }} key={"b"} to="/edit">
+                  edit{" "}
+                </Link>,
+              ]
+            : [
+                <a
+                  style={{ fontSize: "4vw" }}
+                  key={"c"}
+                  onClick={props.doSomething}
+                >
+                  {" "}
+                  Login |
+                </a>,
+                <a
+                  style={{ fontSize: "4vw" }}
+                  key={"d"}
+                  onClick={props.executeScrollForSection2}
+                >
+                  {" "}
+                  Signup
+                </a>,
+              ]}{" "}
+        </li>
 
-				{/* 
+        {/* 
         <li key={5}>
 
             <li key={4}>{props.userState.loggedInStatus == "LOGGED_IN" ? [<Link key={"a"} to="/"> Logout | </Link>, <Link key={"b"} to="/edit">edit </Link>] :   [<Link key={"c"} to="/login"> Login |</Link>, <Link key={"d"} to="/signup"> Signup</Link>]  } </li>
 
         </li>
         */}
-			</UL>
-		</StyledSideMenu>
-	);
+      </UL>
+    </StyledSideMenu>
+  );
 }
 
 export default Menu;
