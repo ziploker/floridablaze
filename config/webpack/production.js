6f6dff349fcecadb1c5e31faa7aa8a4962b184e4
Alejandro Raffo <amsterdamAL@gmail.com>
2024-01-19 01:20:35 -0500

    css fix for production, switch from node-sass to dart

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,10 +1,10 @@
 // The source code including full typescript support is available at:
 // https://github.com/shakacode/react_on_rails_demo_ssr_hmr/blob/master/config/webpack/production.js
 
-import webpackConfig from "./webpackConfig";
+const webpackConfig = require("./webpackConfig");
 
 const productionEnvOnly = (_clientWebpackConfig, _serverWebpackConfig) => {
   // place any code here that is for production only
 };
 
-export default webpackConfig(productionEnvOnly);
+module.exports = webpackConfig(productionEnvOnly);
