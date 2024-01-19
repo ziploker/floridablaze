2453ebd761ec6a09fcad56cd325ae1733c0023bd
amsterdamAL <amsterdamAL@gmail.com>
2024-01-19 15:02:14 -0500

    css not working in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  RAILS_ENV=production
+RAILS_SERVE_STATIC_FILES=enabled
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -21,7 +24,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   ###config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+  ###config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
