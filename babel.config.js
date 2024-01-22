553ad11281432fba6543d88275a2abf3f86aea0b
amsterdamAL <amsterdamAL@gmail.com>
2024-01-22 13:47:53 -0500

    css fixes

--- babel.config.js
+++ babel.config.js
@@ -17,7 +17,7 @@ module.exports = function (api) {
       ],
     ].filter(Boolean),
     plugins: [
-      //process.env.WEBPACK_SERVE && "react-refresh/babel",
+      "react-refresh/babel",
       isProductionEnv && [
         "babel-plugin-transform-react-remove-prop-types",
         {
