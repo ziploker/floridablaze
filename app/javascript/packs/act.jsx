6f6a515dc4135d22b0ef61b37de7f3e7998eb77f
Alejandro Raffo <amsterdamAL@gmail.com>
2024-03-14 01:41:51 -0400

    meta test

--- app/javascript/packs/act.jsx
+++ app/javascript/packs/act.jsx
@@ -2863,7 +2863,7 @@ function Act(props, ref) {
   const { LookupInputRef } = ref;
   const [tester, setTester] = useState("testing");
   const [addressObject, setAddressObject] = useState(null);
-  const [showCards, setShowCards] = React.useState("true");
+  const [showCards, setShowCards] = React.useState("false");
   const [resultFromFlorida, setResultFromFlorida] = React.useState("true");
 
   const [searchButtonActive, setSearchButtonActive] = React.useState(false);
@@ -2961,48 +2961,48 @@ function Act(props, ref) {
   //   },
   // });
 
-  const [results, setResults] = React.useState({
-    one: {
-      resultFromFlorida: "true",
-      name: "Kaylee Tuck",
-      firstName: "Kaylee",
-      lastName: "Tuck",
-      image:
-        "https://www.myfloridahouse.gov//FileStores/Web/Imaging/Member/4776.jpg",
-      id: "ocd-person/7bf7d958-fabd-430b-9326-97586b0c0880",
-      email: "Kaylee.Tuck@myfloridahouse.gov",
-      chamber: "House",
-      party: "Republican",
-      parent: "Florida Legislature",
-      district: "55",
-      fullDistrict: "Florida State House  ",
-      fullDistrictTrunk: "Florida State House",
-      address:
-        "1401 The Capitol; 402 South Monroe Street; Tallahassee, FL 32399-1300",
-      classification: "lower",
-    },
-    two: {
-      name: "Ben Albritton",
-      firstName: "Ben",
-      lastName: "Albritton",
-      image:
-        "https://www.flsenate.gov/PublishedContent/Senators/2020-2022/Photos/s26_5342.jpg",
-      id: "ocd-person/5c81dfe7-1cec-45e8-8044-6d9cd324f2e8",
-      email: "albritton.ben.web@flsenate.gov",
-      chamber: "Senate",
-      party: "Republican",
-      parent: "Florida Legislature",
-      district: "26",
-      fullDistrict: "Florida State Senate  ",
-      fullDistrictTrunk: "Florida State Senate",
-      address:
-        "314 Senate Building; 404 South Monroe Street; Tallahassee, FL 32399-1100",
-      classification: "upper",
-    },
-    hash: "15a8737628b7c84a892c199720cecdeafc7cd07e",
-  });
+  // // const [results, setResults] = React.useState({
+  // //   one: {
+  // //     resultFromFlorida: "true",
+  // //     name: "Kaylee Tuck",
+  // //     firstName: "Kaylee",
+  // //     lastName: "Tuck",
+  // //     image:
+  // //       "https://www.myfloridahouse.gov//FileStores/Web/Imaging/Member/4776.jpg",
+  // //     id: "ocd-person/7bf7d958-fabd-430b-9326-97586b0c0880",
+  // //     email: "Kaylee.Tuck@myfloridahouse.gov",
+  // //     chamber: "House",
+  // //     party: "Republican",
+  // //     parent: "Florida Legislature",
+  // //     district: "55",
+  // //     fullDistrict: "Florida State House  ",
+  // //     fullDistrictTrunk: "Florida State House",
+  // //     address:
+  // //       "1401 The Capitol; 402 South Monroe Street; Tallahassee, FL 32399-1300",
+  // //     classification: "lower",
+  // //   },
+  // //   two: {
+  // //     name: "Ben Albritton",
+  // //     firstName: "Ben",
+  // //     lastName: "Albritton",
+  // //     image:
+  // //       "https://www.flsenate.gov/PublishedContent/Senators/2020-2022/Photos/s26_5342.jpg",
+  // //     id: "ocd-person/5c81dfe7-1cec-45e8-8044-6d9cd324f2e8",
+  // //     email: "albritton.ben.web@flsenate.gov",
+  // //     chamber: "Senate",
+  // //     party: "Republican",
+  // //     parent: "Florida Legislature",
+  // //     district: "26",
+  // //     fullDistrict: "Florida State Senate  ",
+  // //     fullDistrictTrunk: "Florida State Senate",
+  // //     address:
+  // //       "314 Senate Building; 404 South Monroe Street; Tallahassee, FL 32399-1100",
+  // //     classification: "upper",
+  // //   },
+  // //   hash: "15a8737628b7c84a892c199720cecdeafc7cd07e",
+  // // });
 
-  //const [results, setResults] = React.useState({ one: {}, two: {} });
+  const [results, setResults] = React.useState({ one: {}, two: {} });
 
   // // const [results, setResults] = React.useState({
   // // 	one: {
